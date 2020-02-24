7bbb1dbb66f0e668d0ac19aa60900e23c6a331a6
Karim Macknojia <alishahal@msn.com>
2020-02-24 02:49:07 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,9 +5,9 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-          {name:"Benvolio", age:"17", attitute:"worried"},
+          {name:"Benvolio", age:"17", attitude:"worried"},
         
-          {name:"Mercutio", age:"18",  attitute:"hot-headed"}
+          {name:"Mercutio", age:"18",  attitude:"hot-headed"}
           
       ]
    }, 
